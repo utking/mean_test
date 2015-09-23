89785aad87f0dd763e87186c1885572603d0d521
UtkinG <utking@mail.ru>
2015-09-23 19:57:11 +1000

    added find user by ID and get all users

--- app/controllers/users.server.controller.js
+++ app/controllers/users.server.controller.js
@@ -7,8 +7,36 @@ exports.create = function(req, res, next) {
 	user.save(function(err) {
 		if (err) {
 			return next(err);
+		} else {
+			res.json(user);
 		}
-		res.json(user);
+	});
+};
+
+exports.list = function(req, res, next) {
+	User.find({}, 'username lastName email', {skip: 0, limit: 10}, function(err, users) {
+		if (err) {
+			next(err);
+		} else {
+			res.json(users);
+		}
+	});
+};
+
+exports.read = function(req, res, next) {
+	res.json(req.user);
+};
+
+exports.userByID = function(req, res, next, userId) {
+	User.findOne({
+		_id: userId
+	}, function(err, user) {
+		if (err) {
+			next(err);
+		} else {
+			req.user = user;
+			next();
+		}
 	});
 };
 
--- app/routes/users.server.routes.js
+++ app/routes/users.server.routes.js
@@ -1,5 +1,12 @@
 var users = require('../../app/controllers/users.server.controller');
 
 module.exports = function(app) {
-	app.route('/users').post(users.create);
+	app.route('/users').
+		post(users.create).
+		get(users.list);
+
+	app.route('/users/:userId').
+		get(users.read);
+
+	app.param('userId', users.userByID);
 }
